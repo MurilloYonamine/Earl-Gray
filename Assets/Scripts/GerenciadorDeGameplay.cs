8749cef3c6c8420ec12b26e79dc0f03c6ef7ab49
Murillo Gomes Yonamine <murillo.gyonamine@gmail.com>
2024-06-08 09:20:52 -0300

    Decidi usa rum método diferente para trocar de cenas, cada diálogo vai ter uma cena para ele.

--- Assets/Scripts/GerenciadorDeGameplay.cs
+++ Assets/Scripts/GerenciadorDeGameplay.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class GerenciadorDeGameplay : MonoBehaviour
 {
-public void TerminarGameplay()
-{
-    Debug.Log("Gameplay terminada.");
-    // Não há necessidade de chamar GerenciadorDeEventos nesta cena
-}
+    public string nomeCenaDialogos;
 
+    void Start()
+    {
+        // Manter este objeto vivo ao mudar de cena
+        DontDestroyOnLoad(gameObject);
+    }
 
+    public void TerminarGameplay()
+    {
+        Debug.Log("Gameplay terminada.");
+
+        // Voltar para a cena dos diálogos
+        SceneManager.LoadScene(nomeCenaDialogos);
+    }
 
     void Update()
     {
-        // Lógica de teste para terminar a gameplay
+        // Lógica de teste para terminar a gameplay (pode ser substituída pela sua lógica real)
         if (Input.GetKeyDown(KeyCode.T))
         {
             TerminarGameplay();
